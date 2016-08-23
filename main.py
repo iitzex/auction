077708432b4185658b56d930c0795c050a039b9f
iitzex <iitzex@gmail.com>
2016-08-23 17:28:58 +0800

    update for random key

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from bs4 import BeautifulSoup
 from parser import parser
 
 
-def traverse(court, proptype, saletype):
-    addr = "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A03.jsp?3B178602AC1AE85962E6ED69533A1A06=21BB4964157FEB3FC15E26CAB507219C&hsimun=all&ctmd=all&sec=all&saledate1=&saledate2=&crmyy=&crmid=&crmno=&dpt=&minprice1=&minprice2=&saleno=&area1=&area2=&registeno=&checkyn=all&emptyyn=all&rrange=不分&comm_yn=&owner1=&order=odcrm&" + \
+def traverse(court, proptype, saletype, key):
+    addr = "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A03.jsp?" + \
+            key + \
+           "&hsimun=all&ctmd=all&sec=all&saledate1=&saledate2=&crmyy=&crmid=&crmno=&" + \
+           "dpt=&minprice1=&minprice2=&saleno=&area1=&area2=&registeno=&checkyn=all&" + \
+           "emptyyn=all&rrange=不分&comm_yn=&owner1=&order=odcrm&" + \
            "courtX=" + court + "&proptypeX=" + proptype + "&saletypeX=" + saletype + \
            "&pageTotal=13&pageSize=15&rowStart=11&query_typeX=db&"
-    r = requests.get(addr)
 
+    r = requests.get(addr)
     soup = BeautifulSoup(r.text, "html.parser")
     pageTotal = soup.input['value']
     print('pageTotal:' + pageTotal)
@@ -37,16 +41,31 @@ def traverse(court, proptype, saletype):
 
 
 def main():
+    key = get_key()
+
     courts = ["TPD", "PCD", "SLD", "TYD", "SCD", "MLD", "TCD", "NTD", "CHD", "ULD",
               "CYD", "TND", "KSD", "PTD", "TTD", "HLD", "ILD", "KLD", "PHD", "KMD", "LCD"]
     props = ["C52", "C51"]
     sales = ["1", "4", "5"]
 
-    for c in courts:
-        for p in props:
-            for s in sales:
-                traverse(c, p, s)
-    # traverse(courts[0], props[0], sales[2])
+    # for c in courts:
+    #     for p in props:
+    #         for s in sales:
+    #             traverse(c, p, s)
+    traverse(courts[0], props[0], sales[2], key)
+
+
+def get_key():
+    addr = "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A02.jsp?proptype=C52&saletype=1&court=TPD"
+    r = requests.get(addr)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    key = soup.input
+    # print(key['name'])
+    # print(key['value'])
+
+    return key['name'] + "=" + key['value']
+
 
 if __name__ == '__main__':
     main()
